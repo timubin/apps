0fcbb2825f3cdc1f28d293a942b91c2932d3c24e
timubin <mubin3242@gmail.com>
2021-12-05 13:29:03 +0600

    second commit

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ for (let i = 0; i < 3; i++) {
   document.querySelectorAll(".myBox")[i].addEventListener("click", function () {
     let text = this.innerHTML;
 
-    console.log(text);
     document.querySelector("#title").innerHTML = text;
 
     switch (text) {
       case "Abbu":
+        document.querySelector("#showimage").style.display = "inline-block";
         var audio = new Audio("sound/abbu.m4a");
         audio.play();
+
         break;
-      case "Ma ":
+      case "Ammu ":
         var audio = new Audio("sound/ammu.m4a");
         audio.play();
         break;
       case "Zinia ":
         var audio = new Audio("sound/sound/zinia.m4a");
+        audio.play();
+        break;
+      case "Pani ":
+        var audio = new Audio("sound/sound/pani.m4a");
+        audio.play();
+        break;
+      case "Suction ":
+        var audio = new Audio("sound/sound/suction.m4a");
+        audio.play();
+        break;
+      case "Khabar ":
+        var audio = new Audio("sound/sound/Khabar.m4a");
         audio.play();
         break;
     }
